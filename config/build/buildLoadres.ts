29f5a4f686a452f3b2c0047da38e2ed8d2ce706b
Roman R <ggkurwa228@gmail.com>
2025-08-09 22:59:13 +0700

    add react

--- config/build/buildLoadres.ts
+++ config/build/buildLoadres.ts
@@ -1,11 +1,17 @@
 import webpack from "webpack"
 export function buildLoaders(): webpack.RuleSetRule[]{
-    
+
+    const typeScriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+    }
     return [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
+            typeScriptLoader,
+            cssLoader
         ]
 }
