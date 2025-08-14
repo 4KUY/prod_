1cb9b35ca19601044d0826b78e8d118a1e48f854
Roman R <ggkurwa228@gmail.com>
2025-08-15 00:32:45 +0700

    add themes/styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import './index.scss'
-import { Suspense } from "react";
+import './styles/index.scss'
+import { Suspense, useContext, useState } from "react";
 import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
+
 const App = () => {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={'/about'}>about</Link>
       <Link to={'/'}>main</Link>
+      <button onClick={toggleTheme}>Toggle Theme</button>
 
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, {useState, useMemo, FC} from 'react'
+import { LOCAL_STORAGE_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_KEY) as Theme || Theme.LIGHT;
+
+const ThemeProvider:FC = ({children}) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const toggleTheme = () => {
+      setTheme(theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+    }
+    const defaultProps = useMemo(() => ({
+            theme: theme, 
+            setTheme: setTheme
+    }), [theme])
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+        {children}
+    </ThemeContext.Provider>
+  );
+};
+export default ThemeProvider;
