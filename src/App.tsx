a9e5c25760d3ad02b5789290a32509af240ae7d6
Roman R <ggkurwa228@gmail.com>
2025-08-14 19:16:20 +0700

    rrd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Counter } from "./components/Counter";
+import { Route, Routes } from "react-router-dom";
 import './index.scss'
+import { Suspense } from "react";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPageAsync";
 const App = () => {
   return (
     <div className="app">
-        fdf
-      <Counter />
+      <Link to={'/about'}>about</Link>
+      <Link to={'/'}>main</Link>
+
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path={'/'} element={<AboutPageAsync />} />
+          <Route path={'/about'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
