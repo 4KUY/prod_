bff524afbde7b0b2d6dda994154d6b3569853711
Roman R <ggkurwa228@gmail.com>
2025-08-23 09:34:06 +0700

    routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classnames/classNames";
 import { AboutPage } from "pages/AboutPage";
 import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 
 const App = () => {
@@ -16,12 +17,7 @@ const App = () => {
       <Link to={'/'}>main</Link>
       <button onClick={toggleTheme}>Toggle Theme</button>
 
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path={'/'} element={<AboutPage />} />
-          <Route path={'/about'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter/>
     </div>
   );
 };
--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -2,5 +2,5 @@ import ThemeProvider from "./ThemeProvider/ui/ThemeProvider";
 import { useTheme } from "./ThemeProvider/lib/useTheme";
 export {
     ThemeProvider,
-    useTheme
+    useTheme,
 }
