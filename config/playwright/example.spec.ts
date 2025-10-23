f5273a6c7518e91b8d3e53f790fe9b3f65516176
Roman R <ggkurwa228@gmail.com>
2025-10-23 17:36:11 +0700

    threshold

--- config/playwright/example.spec.ts
+++ config/playwright/example.spec.ts
@@ -31,7 +31,7 @@ test.describe('Storybook visual tests - all stories', () => {
             });
   
             const screenshot = await page.screenshot();
-            expect(screenshot).toMatchSnapshot(`${story.id}.png`);
+            expect(screenshot).toMatchSnapshot(`${story.id}.png`, { threshold: 0.02 });
         });
     }
 });
